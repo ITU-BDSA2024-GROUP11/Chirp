edfd9560c56b5ae2a7185196ae367c5b188bf1bd
BastianGram <bapo@itu.dk>
2024-08-28 15:25:25 +0200

    Now reads the file
    Co-authored by: Frederik fbuu@itu.dk
    Co-authored by: Oliver olibr@itu.dk
    Co-authored by: Jon jble@itu.dk
    Co-authored by: Lisa lish@itu.dk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿using System.IO;
+
+String line;
+try
+{
+    //Pass the file path and file name to the StreamReader constructor
+    StreamReader sr = new StreamReader("..\\..\\..\\chirp_cli_db.csv");
+    //Read the first line of text
+    line = sr.ReadLine();
+    //Continue to read until you reach end of file
+    while (line != null)
+    {
+        //write the line to console window
+        Console.WriteLine(line);
+        //Read the next line
+        line = sr.ReadLine();
+    }
+    //close the file
+    sr.Close();
+    Console.ReadLine();
+}
+catch(Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Executing finally block.");
+}
