0fdbe28263ed3076f943ef70f59242de4d58d784
BastianGram <bapo@itu.dk>
2024-09-03 13:35:22 +0200

    Completed the task
    We made the program correctly read the file, and added a write function.
    Both can be called from the terminal using dotnet run -- cheep "Hello, world!"
    and dotnet run -- read
    Co-authored by: Frederik fbuu@itu.dk
    Co-authored by: Oliver olibr@itu.dk
    Co-authored by: Jon jble@itu.dk
    Co-authored by: Lisa lish@itu.dk

--- Program.cs
+++ Program.cs
@@ -1,29 +1,66 @@
 ﻿using System.IO;
+using System;
 
-String line;
-try
+if (args[0] == "read")
+{
+    readFile();
+}
+else if (args[0] == "cheep")
+{
+    writeToFile(args[1]);
+}
+
+static void readFile()
 {
     //Pass the file path and file name to the StreamReader constructor
-    StreamReader sr = new StreamReader("..\\..\\..\\chirp_cli_db.csv");
+    StreamReader sr = new StreamReader("./chirp_cli_db.csv");
     //Read the first line of text
-    line = sr.ReadLine();
+    String line = sr.ReadLine();
     //Continue to read until you reach end of file
     while (line != null)
     {
+        if (line.Equals("Author  Message	Timestamp"))
+        {
+            line = sr.ReadLine();
+            continue;
+        }
+
         //write the line to console window
-        Console.WriteLine(line);
+        string[] words = line.Split(new char[] { '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var user = (words[0]);
+
+
+        var message = string.Join(" ", words, 1, words.Length - 2);
+
+        int i = 0;
+        string s = words[words.Length - 1];
+        int.TryParse(s, out i);
+        string timeInDate = (UnixToDate(i));
+
+        Console.WriteLine(user + " @ " + timeInDate + ": " + message);
         //Read the next line
         line = sr.ReadLine();
     }
     //close the file
     sr.Close();
-    Console.ReadLine();
 }
-catch(Exception e)
+
+static void writeToFile(string message)
 {
-    Console.WriteLine("Exception: " + e.Message);
+    using (StreamWriter sw = new StreamWriter("./chirp_cli_db.csv", append: true))
+    {
+        sw.WriteLine();
+        string username = Environment.UserName;
+        //sw.WriteLine(content);
+        long unixTime=DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        sw.Write(username + "\t" + message + "\t" + unixTime);
+        sw.Close();
+    }
 }
-finally
+
+static String UnixToDate(Int32 unixTime)
 {
-    Console.WriteLine("Executing finally block.");
+    return DateTimeOffset.FromUnixTimeSeconds(unixTime).ToLocalTime().DateTime.ToString("MM/dd/yy HH:mm:ss").Replace('.',':');
+    
 }
+
