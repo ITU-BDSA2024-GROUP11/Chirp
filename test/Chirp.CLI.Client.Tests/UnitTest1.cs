9b722a4a429b9af1e3ec3ff42738353c7c9c1beb
Oliver Brinch <olibr@itu.dk>
2024-09-18 15:57:54 +0200

    Updated itemgroups & removed irrelevant test
    Co-authored by: Frederik fbuu@itu.dk

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -16,20 +16,5 @@ namespace Chirp.CLI.Client.Tests
 
             Assert.Equal(expectedDate, result);
         }
-
-        [Fact]
-        public void TestPrintCheeps(){
-            
-            string msg = "Author,Message,Timestamp
-        ropf,"Hello, BDSA students!",1690891760
-        adho,"Welcome to the course!",1690978778
-        adho,"I hope you had a good summer.",1690979858
-        ropf,"Cheeping cheeps on Chirp :)",1690981487";
-            
-            CsvDatabase<Cheep> db = new CsvDatabase<Cheep>();
-            string dbresult = UserInterface.ReadCheeps();
-
-        }
-    
     }
 }
