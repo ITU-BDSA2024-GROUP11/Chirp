0553ddc307bc1f1e223b2d91d2491f1d1e68e648
B3astian <151624982+B3astian@users.noreply.github.com>
2024-09-18 20:40:05 +0200

    Update Program.cs

--- src/Chirp.CLI.Client/Program.cs
+++ src/Chirp.CLI.Client/Program.cs
@@ -25,6 +25,10 @@ Parser.Default.ParseArguments<ReadOptions, CheepOptions>(args)
     .WithParsed<ReadOptions>(opts => ReadCheeps())
     .WithParsed<CheepOptions>(opts => StoreCheep(opts.Message));
 
+    //This is so that the commando prompt doesn't close
+    Console.WriteLine("Press Enter to exit...");
+    Console.ReadLine();
+
 
 // Options for the 'read' command
 [Verb("read", HelpText = "Read all stored cheeps.")]
@@ -39,7 +43,6 @@ class ReadOptions
 
 class CheepOptions
 {
-    //comment
     [Value(0, MetaName = "message", HelpText = "The message to store.", Required = true)]
     public string Message { get; set; }
 }
