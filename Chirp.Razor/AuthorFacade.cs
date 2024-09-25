50ca65c23782aabe3393e1f5426e9e3d33485e7a
Jon <jble@itu.dk>
2024-09-25 17:44:31 +0200

    Writing AuthorFacade with Class and constructor Co-authored by: Oliver olibr@itu.dk

--- Chirp.Razor/AuthorFacade.cs
+++ Chirp.Razor/AuthorFacade.cs
@@ -1,21 +1,31 @@
-using System.Data;
 using Microsoft.Data.Sqlite;
 
-var sqlDBFilePath = "/tmp/chirp.db";
-var AuthorQuery = "SELECT * FROM message, user" + "WHERE message.author_id = user.user_id and user.username = @Author;";
-// ORDER by message.pub_date desc (order stuff)
-string author = "Adrian"; //Has to be a call from a method that takes input from user
-using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+public class AuthorFacade
 {
-    connection.Open();
+    string sqlDBFilePath = "/tmp/chirp.db";
 
-    var command = connection.CreateCommand();
-    command.CommandText = AuthorQuery;
-    command.Parameters.AddWithValue("@Author", author);
+    string AuthorQuery = "SELECT * FROM message, user" +
+                         "WHERE message.author_id = user.user_id and user.username = @Author";
+    SqliteConnection connection;
 
-    using var reader = command.ExecuteReader();
-    while (reader.Read())
+// ORDER by message.pub_date desc (order stuff)
+    public AuthorFacade()
     {
-    
+        using (connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+        }
+    }
+
+    public void GetCheepsFromAuthor(string author)
+    {
+        var command = connection.CreateCommand(); 
+        command.CommandText = AuthorQuery;
+        
+        using var reader = command.ExecuteReader(); 
+        while (reader.Read())
+        {
+            
+        }
     }
 }
