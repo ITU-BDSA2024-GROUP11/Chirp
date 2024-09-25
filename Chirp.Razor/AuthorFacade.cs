aee9cf12359f7ece27f1a99b0c7f1f6de40d0177
Oliver Brinch <olibr@itu.dk>
2024-09-25 15:59:25 +0200

    Update Author.cs
    String command paramater stuff
    
    Co-Authored-By: Jon <108537143+Borgodorgo@users.noreply.github.com>

--- Chirp.Razor/AuthorFacade.cs
+++ Chirp.Razor/AuthorFacade.cs
@@ -1,14 +1,17 @@
-
+using System.Data;
+using Microsoft.Data.Sqlite;
 
 var sqlDBFilePath = "/tmp/chirp.db";
-var AuthorQuery = @"SELECT * FROM message, user" + "WHERE message.author_id = user.user_id and user.username = @Author";
+var AuthorQuery = "SELECT * FROM message, user" + "WHERE message.author_id = user.user_id and user.username = @Author;";
 // ORDER by message.pub_date desc (order stuff)
+string author = "Adrian"; //Has to be a call from a method that takes input from user
 using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
 {
     connection.Open();
 
     var command = connection.CreateCommand();
     command.CommandText = AuthorQuery;
+    command.Parameters.AddWithValue("@Author", author);
 
     using var reader = command.ExecuteReader();
     while (reader.Read())
