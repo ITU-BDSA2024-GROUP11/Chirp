5d798a4cfb0d81070e3060ae452d2925050ef92c
Jon <jble@itu.dk>
2024-10-01 10:37:34 +0200

    Using Sqlite3 for storing Cheeps

--- Chirp.Razor/AuthorFacade.cs
+++ Chirp.Razor/AuthorFacade.cs
@@ -4,8 +4,8 @@ public class AuthorFacade
 {
     string sqlDBFilePath = "/tmp/chirp.db";
 
-    string AuthorQuery = "SELECT * FROM message, user" +
-                         "WHERE message.author_id = user.user_id and user.username = @Author";
+    string AuthorQuery = @"SELECT username, text, pub_date FROM message, user WHERE author_id = user_id and username = @Author ORDER by message.pub_date desc";
+    string AllCheepsQuery = @"SELECT username, text, pub_date FROM message, user WHERE author_id = user_id ORDER by message.pub_date desc";
     SqliteConnection connection;
 
 // ORDER by message.pub_date desc (order stuff)
@@ -17,15 +17,34 @@ public class AuthorFacade
         }
     }
 
-    public void GetCheepsFromAuthor(string author)
+    public List<Cheeps> GetCheeps()
     {
-        var command = connection.CreateCommand(); 
-        command.CommandText = AuthorQuery;
-        
-        using var reader = command.ExecuteReader(); 
+        connection.Open();
+        var cheeps = new List<Cheeps>();
+        using var command = new SqliteCommand(AllCheepsQuery, connection);
+        using var reader = command.ExecuteReader();
         while (reader.Read())
         {
-            
+            cheeps.Add(new Cheeps(reader.GetString(0), reader.GetString(1), reader.GetString(2)));
         }
+        return cheeps;
+    }
+
+    public List<Cheeps> GetCheepsFromAuthor(string author)
+    {
+        connection.Open();
+        using var command = new SqliteCommand(AuthorQuery, connection);
+        command.Parameters.Add("@Author", SqliteType.Text); 
+        command.Parameters["@Author"].Value = author;
+        Console.WriteLine("connection is " + connection.State);
+        Console.WriteLine(command.CommandText);
+        using var reader = command.ExecuteReader();
+        var cheeps = new List<Cheeps>();
+        while (reader.Read()) 
+        { 
+            cheeps.Add(new Cheeps(reader.GetString(0), reader.GetString(1), reader.GetString(2)));
+        }
+
+        return cheeps;
     }
 }
--- Chirp.Razor/CheepService.cs
+++ Chirp.Razor/CheepService.cs
@@ -1,29 +1,34 @@
-public record CheepViewModel(string Author, string Message, string Timestamp);
+public record Message(int message_id, int author_id, string message, int pub_date);
+public record User(int user_id, string username, string email);
+public record Cheeps(string username, string message, string pub_date);
 
 public interface ICheepService
 {
-    public List<CheepViewModel> GetCheeps();
-    public List<CheepViewModel> GetCheepsFromAuthor(string author);
+    public List<Cheeps> GetCheeps();
+    public List<Cheeps> GetCheepsFromAuthor(string author);
 }
 
 public class CheepService : ICheepService
 {
     // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
+    private static readonly List<Cheeps> _cheeps = new()
         {
-            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
-            new CheepViewModel("Adrian", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
+            new Cheeps("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
+            new Cheeps("Adrian", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
         };
 
-    public List<CheepViewModel> GetCheeps()
+    public List<Cheeps> GetCheeps()
     {
-        return _cheeps;
+        AuthorFacade authorFacade = new AuthorFacade();
+        return authorFacade.GetCheeps();
     }
 
-    public List<CheepViewModel> GetCheepsFromAuthor(string author)
+    public List<Cheeps> GetCheepsFromAuthor(string author)
     {
         // filter by the provided author name
-        return _cheeps.Where(x => x.Author == author).ToList();
+        //return _cheeps.Where(x => x.Author == author).ToList();
+        AuthorFacade authorFacade = new AuthorFacade();
+        return authorFacade.GetCheepsFromAuthor(author);
     }
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
