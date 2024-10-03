90ab6d4db9f73d32bba5d33a925aa2301a7001af
BastianGram <bapo@itu.dk>
2024-10-03 12:33:29 +0200

    Workflow run

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -14,7 +14,7 @@ public class DBFacade
     string PageQuery = @"SELECT username, text, pub_date FROM message, user WHERE author_id = user_id ORDER by message.pub_date desc LIMIT 32 OFFSET @PageOffset";
     SqliteConnection connection;
     
-    public DBFacade()
+    public DBFacade()//
     {
         if (dbpath == null) dbpath = "./tmp/chirp.db";
         
