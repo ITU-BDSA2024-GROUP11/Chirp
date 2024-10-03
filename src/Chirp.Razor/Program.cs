f1aa814d6fc2f27fcafd8139219a5fb1ac4a7469
BastianGram <bapo@itu.dk>
2024-10-03 13:15:21 +0200

    azure

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// This sets the app to listen on port 8080 (you can replace with your port)
-//builder.WebHost.UseUrls("http://localhost:8080");
-
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<ICheepService, CheepService>();
