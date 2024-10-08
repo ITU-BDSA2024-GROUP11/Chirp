1f0f2ba48e0099c13210d90d3e5f87742ef177e7
Frederik Buur <fbuu@itu.dk>
2024-10-08 15:24:21 +0200

    Made GetCheeps
    Co-Authored-By: Bastian Gram Poulsgaard <172143336+BastianGram@users.noreply.github.com>

--- src/Chirp.Razor/CheepRepository.cs
+++ src/Chirp.Razor/CheepRepository.cs
@@ -1,14 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Chirp.Razor;
 
 public interface ICheepRepository
 {
+    public IEnumerable<Cheep> GetCheeps(int skip = 0, string? authorUsername = null);
 }
 public class CheepRepository : ICheepRepository
 {
     private readonly ChirpDBContext _dbContext;
+    private int size = 32;
     public CheepRepository(ChirpDBContext dbContext)
     {
         _dbContext = dbContext;
     }
+
+    public IEnumerable<Cheep> GetCheeps(int skip = 0, string? authorUsername = null)
+    {
+        var query = _dbContext.Cheeps.Include(cheep => cheep.Author)
+            .AsQueryable();
+
+        // Apply author filtering if the username is provided
+        if (!string.IsNullOrEmpty(authorUsername))
+        {
+            query = query.Where(cheep => cheep.Author.Name == authorUsername);
+        }
+
+        // Skip and take based on the parameters
+        query = query.Skip(skip);
+       
+
+        return query.ToList();
+    }
+
+
+
+    
 
 }
