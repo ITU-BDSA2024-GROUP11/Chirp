43294643370d5dc93521c4632c3bd69b493813c4
Frederik Buur <fbuu@itu.dk>
2024-10-09 15:27:52 +0200

    Added AddCheep

--- src/Chirp.Razor/CheepRepository.cs
+++ src/Chirp.Razor/CheepRepository.cs
@@ -3,9 +3,11 @@ namespace Chirp.Razor;
 public interface ICheepRepository
 {
 }
+
 public class CheepRepository : ICheepRepository
 {
     private readonly ChirpDBContext _dbContext;
+
     public CheepRepository(ChirpDBContext dbContext)
     {
         _dbContext = dbContext;
@@ -20,10 +22,29 @@ public class CheepRepository : ICheepRepository
             TimeStamp = cheep.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")
         };
     }
+
+    public void AddCheep(string text, int authorId)
+    {
+        // Create a cheep object
+        var newCheep = new Cheep
+        {
+            AuthorId = authorId,
+            Author = FindAutoherById(authorId),
+            Text = text,
+            TimeStamp = DateTime.Now
+        };
+
+        // Add the new Cheep to the DbSet
+        _dbContext.Cheeps.Add(newCheep);
+
+        // Save changes to the database
+        _dbContext.SaveChanges();
+    }
 }
+
 public class CheepDTO
 {
     public string Text { get; set; }
     public string Author { get; set; }
     public string TimeStamp { get; set; }
-};
+}
