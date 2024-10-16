d49502330fc2bd27ff380fc6cfd4cb3f12e62075
Frederik Buur <fbuu@itu.dk>
2024-10-16 11:33:55 +0200

    Update PaginationTests.cs

--- test/Chirp.Infrastructure.Tests/PaginationTests.cs
+++ test/Chirp.Infrastructure.Tests/PaginationTests.cs
@@ -1,46 +1,52 @@
-// namespace Chirp.Razor.Tests;
-//
-// public class PaginationTests
-// {
-//     [Fact]
-//     public void PaginationTest()
-//     {
-//         var dbFacade = new DBFacade();
-//         var cheeps = dbFacade.GetCheepsFromPage(1);
-//         Assert.Equal(32, cheeps.Count);
-//     }
-//
-//     [Fact]
-//     public void PaginationUserTest()
-//     {
-//         var dbFacade = new DBFacade();
-//         var cheeps = dbFacade.GetCheepsFromPage(1);
-//         Assert.Equal("Jacqualine Gilcoine", cheeps[0].username);
-//     }
-//
-//     [Fact]
-//     public void PaginationUserPageTest()
-//     {
-//         var dBFacade = new DBFacade();
-//         var cheeps = dBFacade.GetCheepsFromAuthorPage("Jacqualine Gilcoine", 2);
-//         Assert.Equal("Jacqualine Gilcoine", cheeps[0].username);
-//         Assert.Equal("What a relief it was the place examined.", cheeps[0].message);
-//     }
-//
-//     [Fact]
-//     public void PaginationSecondPageTest()
-//     {
-//         var dbFacade = new DBFacade();
-//         var cheeps = dbFacade.GetCheepsFromPage(2);
-//         Assert.Equal("Nathan Sirmon", cheeps[1].username);
-//     }
-//
-//     [Fact]
-//     public void DBPathNullToDefaultTest()
-//     {
-//         var dbFacade = new DBFacade();
-//         Assert.Equal("/tmp/chirp.db", dbFacade.getDbPath());
-//     }
-// }
+using Chirp.Infrastructure;
+using Chirp.Infrastructure.Chirp.Repositories;
+using Chirp.Infrastructure.DataModel;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
-namespace Chirp.Infrastructure.Tests;
+namespace Chirp.Razor.Tests;
+
+public class PaginationTests
+{
+    private readonly CheepRepository _repository;
+
+    public PaginationTests()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var options = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection).Options;
+        var context = new ChirpDBContext(options);
+        context.Database.EnsureCreated();
+        DbInitializer.SeedDatabase(context);
+        _repository = new CheepRepository(context);
+    }
+
+    [Fact]
+    public void PaginationTest()
+    {
+        var cheeps = _repository.GetCheeps(1);
+        Assert.Equal(32, cheeps.Count);
+    }
+
+    [Fact]
+    public void PaginationUserTest()
+    {
+        var cheeps = _repository.GetCheeps(1, "Jacqualine Gilcoine");
+        Assert.Equal("Jacqualine Gilcoine", cheeps[0].Author);
+    }
+
+    [Fact]
+    public void PaginationUserPageTest()
+    {
+        var cheeps = _repository.GetCheeps(2, "Jacqualine Gilcoine");
+        Assert.Equal("Jacqualine Gilcoine", cheeps[0].Author);
+        Assert.Equal("What a relief it was the place examined.", cheeps[0].Text);
+    }
+
+    [Fact]
+    public void PaginationSecondPageTest()
+    {
+        var cheeps = _repository.GetCheeps(2);
+        Assert.Equal("Nathan Sirmon", cheeps[1].Author);
+    }
+}
