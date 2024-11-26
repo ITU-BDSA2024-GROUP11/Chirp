ec47138305990bf4d017f893a1ac5b6c34e91b2e
Frederik Buur <fbuu@itu.dk>
2024-11-26 13:28:54 +0100

    Update AuthorDTO.cs
    Co-Authored-By: Jon <108537143+Borgodorgo@users.noreply.github.com>

--- src/Chirp.Core/DTO/AuthorDTO.cs
+++ src/Chirp.Core/DTO/AuthorDTO.cs
@@ -5,5 +5,5 @@ public class AuthorDTO
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string Id { get; set; }
-    public required List<AuthorDTO> Follows { get; set; } = [];
+    public List<AuthorDTO> Follows { get; set; } = [];
 }
