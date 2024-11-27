058dc27f3a90cdd5a4c4f193d231ed0142376771
Frederik Buur <fbuu@itu.dk>
2024-11-27 11:24:08 +0100

    Removed findbyMail, Removed unused buttons and removed extra conforamtion pages

--- src/Chirp.Core/RepositoryInterfaces/IAuthorRepository.cs
+++ src/Chirp.Core/RepositoryInterfaces/IAuthorRepository.cs
@@ -5,7 +5,6 @@ namespace Chirp.Core.RepositoryInterfaces;
 public interface IAuthorRepository
 {
     AuthorDTO GetAuthorByName(string name);
-    AuthorDTO GetAuthorByEmail(string email);
     void CreateAuthor(string name, string email);
     string GetAuthorID(string username);
     void FollowAuthor(string userId, string followId);
--- src/Chirp.Infrastructure/Chirp.Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/AuthorRepository.cs
@@ -32,13 +32,6 @@ public class AuthorRepository : IAuthorRepository
         return authorDTO;
     }
 
-    public AuthorDTO GetAuthorByEmail(string email)
-    {
-        var result = FindAuthorByEmail(email);
-        var authorDTO = AuthorToDTO(result);
-        return authorDTO;
-    }
-
     public void CreateAuthor(string name, string email)
     {
         var author = new Author
@@ -94,15 +87,6 @@ public class AuthorRepository : IAuthorRepository
             where author.UserName == name
             select author;
         var result = query.First();
-        return result;
-    }
-
-    private Author FindAuthorByEmail(string email)
-    {
-        var query = from author in _dbContext.Authors
-            where author.Email == email
-            select author;
-        var result = query.FirstOrDefault();
         return result;
     }
 
